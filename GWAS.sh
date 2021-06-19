e3b1b38a2437f8e4c4c92db513eaba127891edbb
EmmaUoM <80395634+EmmaUoM@users.noreply.github.com>
2021-06-19 22:05:27 +0800

    Create GWAS.sh

---
+++ GWAS.sh
@@ -0,0 +1,4 @@
+# focusing on autosomal SNPs
+# covariates with first 3 MDS components
+# GWAS results compared with "Genome-wide association study identifies variants at CLU and PICALM associated with Alzheimer's disease"
+
