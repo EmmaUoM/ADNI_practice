aaa42cea3238766cf297d00e03b7b3cd4ba01899
EmmaUoM <80395634+EmmaUoM@users.noreply.github.com>
2021-06-22 21:47:22 +0800

    Update and rename GWAS.sh to GWAS+PRS.sh

--- GWAS+PRS.sh
+++ GWAS+PRS.sh
@@ -1,8 +1,9 @@
+## GWAS
 # Extract autosomal SNP
 awk '{ if ($1 >= 1 && $1 <= 22) print $2 }' hweclean.bim > autosomal_snp.txt
 plink --bfile hweclean --extract autosomal_snp.txt --make-bed --out autosomal
 
-# Extract SNP from Nature paper
+# Extract SNP from Nature paper: Common polygenic variation contributes to risk of schizophrenia and bipolar disorder
 plink --bfile autosomal --extract SNP_ID.txt --make-bed --out nature
 
 # generate cov file from mds
@@ -13,5 +14,6 @@ awk '{print$1, $2, $4, $5, $6}' mds.mds > covar_mds.txt
 # GWAS with covariates
 plink --bfile nature --covar covar_mds.txt --logistic --hide-covar --out gwas 
 
+## PRS
 # LD based clumping
 
